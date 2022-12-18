ed40230a81b38cf0862413728f21dd8df8eb9e5a
Swasti-123 <86404753+Swasti-123@users.noreply.github.com>
2022-12-18 19:04:58 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -23,10 +23,20 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video= createCapture(VIDEO);
+	video.size(700,600);
+	poseNet= ml5.poseNet(video, modelLoaded);
+	poseNet.on( 'poses', gotPoses);
 }
+
+function modelLoaded(){
+  console.log("model has loaded");
+  }
 
 
 function draw(){
+	image(video, 0, 0, 700, 600);
 
  background(0); 
 
